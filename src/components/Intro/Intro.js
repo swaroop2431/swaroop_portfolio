64d4cc9d48c10209efbda3db7de8143a93bd7277
Swaroop@2431 <saiswaroop5521@gmail.com>
2023-12-12 05:30:31 +0530

    Resume Download added

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,30 +1,3 @@
-// import React from 'react'
-// import { Link } from 'react-scroll'
-// import './Intro.css'
-// import bg from '../../assets/image.png'
-// import btnImg from '../../assets/hireme.png'
-
-// function Intro() {
-
-
-
-//   return (
-//     <section id='intro'>
-//       <div className="introContent">
-//         <span className='hello'>Hello,</span>
-//         <span className='introText'>I'm <span className='introName'>Tipparthi Sai Swaroop</span><br/>Web Developer</span>
-//         <p className="introPara">I am a Web Developer with experience in creating <br /> visually appealing and user friendly websites.</p>
-        // <Link><button className='btn'><img src={btnImg} alt="Hire Me" className='btnImg'/>Hire Me</button></Link>
-//       </div>
-//       <img src={bg} alt="Profile" className='bg'/>
-//     </section>
-//   )
-// }
-
-// export default Intro
-
-
-
 import React from 'react';
 import { Link } from 'react-scroll';
 import './Intro.css';
